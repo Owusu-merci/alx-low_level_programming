8ce802903ee5a83b60f7f47c5ff7e13a3e04e456
Owusu-merci <owusumerci123@gmail.com>
2022-12-11 09:44:59 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,11 @@
  *
  * return: always 0 (success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcomr is a piece of art,\n");
 	return (0);
+
 }
 
