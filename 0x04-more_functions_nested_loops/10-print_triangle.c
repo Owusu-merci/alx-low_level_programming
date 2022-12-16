3f22ab8567d83198ec89274a07c43619eeef8fde
Owusu-merci <owusumerci123@gmail.com>
2022-12-16 10:18:30 -0800

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,31 +2,29 @@
 
 /**
  * print_triangle - Prints a triangle, using the character #.
- *  @size: The size of the triangle.
+ * @size: The size of the triangle.
  */
 
 void print_triangle(int size)
 {
+	int hash, index;
 
-		int hash, index;
-
-		if (size > 0)
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
 		{
-			for (hash = 1; hash <= size; hash++)
-			{
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
 
-							for (index = size - hash; index > 0; index--)
-								_putchar(' ');
+			for (index = 0; index < hash; index++)
+				_putchar('#');
 
-							for (index = 0; index < hash; index++)
-								_putchar('#');
+			if (hash == size)
+				continue;
 
-							if (hash == size)
-								continue;
-
-							_putchar('\n');
-			}
+			_putchar('\n');
 		}
+	}
 
-		putchar('\n');
+	_putchar('\n');
 }
