6861baf263f70b8a4fc6e4c3f6f729396fff49a7
Owusu-merci <owusumerci123@gmail.com>
2023-05-18 21:16:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Discription: prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0(success).
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
