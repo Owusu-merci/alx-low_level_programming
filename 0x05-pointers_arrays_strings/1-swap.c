d89bc223366727b3f0af20e81b1fc0c3a91b2b67
Owusu-merci <owusumerci123@gmail.com>
2023-05-29 18:19:17 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int -  Write a function that swaps the values of two integers.
+ * @a: An integer pointer in function.
+ * @b: An integer pointer in function.
+ * Return: void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Solutions to the tasks in 0x05-pointers_arrays_strings
