48a034544293dc5e770bed96eb9aade62d9be7df
Owusu-merci <owusumerci123@gmail.com>
2023-06-12 04:15:48 -0700

    arg1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: Always 0(success).
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int  argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
